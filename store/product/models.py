20087b6466d0afeb8f2432fbcd2732beabb47845
BekOsu <95960598+BekOsu@users.noreply.github.com>
2022-03-23 17:24:07 +0400

    last

--- store/product/models.py
+++ store/product/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,8 +10,3 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
-class Question(models.Model):
-    # ...
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
