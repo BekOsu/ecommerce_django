e70db592a970ec727a3da3bae53fda543e3deac1
BekOsu <95960598+BekOsu@users.noreply.github.com>
2022-03-24 22:41:30 +0400

    collectstatic

--- store/store/settings.py
+++ store/store/settings.py
@@ -120,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'store/static')
+                  
+                  ] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
