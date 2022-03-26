bcba38de6b35ab4bc8931b74d1a4301bae373b1e
BekOsu <95960598+BekOsu@users.noreply.github.com>
2022-03-26 12:28:14 +0400

    add media

--- store/store/settings.py
+++ store/store/settings.py
@@ -127,6 +127,14 @@ STATICFILES_DIR = [
                   
                   ] 
 
+#MEDIA_URL 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- store/store/urls.py
+++ store/store/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from store.settings import MEDIA_URL
 
 urlpatterns = [
     path('', include('product.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
