e116eb6a9fa613dc84b70643fbfa899fdf21e927
BekOsu <95960598+BekOsu@users.noreply.github.com>
2022-03-28 15:35:33 +0400

    add form m3

--- store/about/models.py
+++ store/about/models.py
@@ -2,6 +2,8 @@ from warnings import catch_warnings
 from django.db import models
 
 
+
+
 class Login(models.Model):
     username = models.CharField(max_length=50)   
     password = models.CharField(max_length=50)   
